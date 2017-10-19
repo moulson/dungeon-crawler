f52b2500b127c195d6cc42594363aaa72d2af087
Daniel Moulson <dan24797@gmail.com>
2017-10-19 02:26:34 +0100

    added UI for weapon machine, made UI toggle, made UI disappear once machine had been used

--- Assets/World/Prefabs/WeaponBox/Scripts/CollisionDetector.cs
+++ Assets/World/Prefabs/WeaponBox/Scripts/CollisionDetector.cs
@@ -7,20 +7,44 @@ public class CollisionDetector : MonoBehaviour {
 	private bool weaponSpawned = false;
 	public AudioClip leverPullClip;
 	public List<GameObject> theWeapons;
+	public GameObject uiPrompt;
+	private bool isShowing = true;
+	private bool hasSpawnedUI = false;
+	private bool hasDestroyedUI = false;
 	void OnTriggerEnter(){
-		inRange = true;
+		if(!hasSpawnedUI){
+			//Create the ui object, but only ever once.
+			uiPrompt = Instantiate(uiPrompt);
+			hasSpawnedUI = true;
+		}
+		if(isShowing && !hasDestroyedUI){
+			//set state to in range, also activate the UI
+			inRange = true;
+			uiPrompt.SetActive(isShowing);
+		}
 	}
 	void OnTriggerExit(){
+		//when not looking/being near the object, make sure the UI isn't showing
 		inRange = false;
+		if(!hasDestroyedUI)
+			uiPrompt.SetActive(false);
+		
 	}
 	void Update(){
 		if(Input.GetKeyDown(KeyCode.E) && inRange && !weaponSpawned){
 			SpawnWeapon();
+			isShowing = false;
+			//Clean up this mess afterwards
+			Destroy(uiPrompt,0.5f);
+			hasDestroyedUI = true;
 		}
 	}
 	void SpawnWeapon(){
+		//Play the sound of rolling the machine.
 		AudioSource.PlayClipAtPoint(leverPullClip, this.transform.position);
+		//Create the weapon object.
 		Instantiate(theWeapons[RandomNumber(0, theWeapons.ToArray().Length)], new Vector3(this.transform.position.x, this.transform.position.y + 1, this.transform.position.z), this.transform.rotation);
+		//Set the state to make sure machine won't be used again
 		weaponSpawned = true;
 	}
 	int RandomNumber(int min, int max){
@@ -29,6 +53,7 @@ public class CollisionDetector : MonoBehaviour {
 			return randInt.Next(min, max);
 		}
 		catch{
+			//Failsafe in case of oob
 			return randInt.Next(min, max - 1);
 		}
 	}
